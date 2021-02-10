e04f62ca11cf59c1a67e06e5b91a75c8a6a507b9
Matt Clarke <matt.clarke@esss.se>
2021-02-10 10:12:13 +0100

    Test the sample information for more details

--- integration_tests/test_querying_proposal_system.py
+++ integration_tests/test_querying_proposal_system.py
@@ -131,3 +131,4 @@ def test_get_sample_details_by_proposal_id():
 
     assert len(results) == 2
     assert "questionary" in results[0] and "questionary" in results[1]
+    assert len(results[0]["questionary"]["steps"]) == 3
