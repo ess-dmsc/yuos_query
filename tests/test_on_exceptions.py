235a05b17fa2c93c637b075c54e9de1027028158
Matt Clarke <matt.clarke@esss.se>
2021-02-19 11:18:04 +0100

    Commented out no longer needed test

--- tests/test_on_exceptions.py
+++ tests/test_on_exceptions.py
@@ -14,7 +14,7 @@ from .test_proposal_system_behaviour import (
 SOME_URL = "https://something.com"
 SOME_USER = "account@ess.eu"
 SOME_PASSWORD = "apassword"
-SOME_TOKEN = {"login": {"token": "eyJhbG"}}
+SOME_TOKEN = "not_a_real_token"
 VALID_PROPOSAL_ID = 169
 YMIR_INFO = (4, "YMIR")
 
@@ -23,7 +23,9 @@ def test_issue_with_getting_instrument_data_from_system_raises_correct_exception
     mocked_impl = generate_standard_mock()
     mocked_impl.get_instrument_data.side_effect = TransportServerError("oops")
 
-    proposal_system = YuosClient(SOME_URL, SOME_USER, SOME_PASSWORD, mocked_impl)
+    proposal_system = YuosClient(
+        SOME_URL, SOME_USER, SOME_PASSWORD, mocked_impl, SOME_TOKEN
+    )
 
     with pytest.raises(ConnectionException):
         proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
@@ -33,29 +35,33 @@ def test_issue_with_getting_proposal_data_from_system_raises_correct_exception_t
     mocked_impl = generate_standard_mock()
     mocked_impl.get_proposal_for_instrument.side_effect = TransportServerError("oops")
 
-    proposal_system = YuosClient(SOME_URL, SOME_USER, SOME_PASSWORD, mocked_impl)
+    proposal_system = YuosClient(
+        SOME_URL, SOME_USER, SOME_PASSWORD, mocked_impl, SOME_TOKEN
+    )
 
     with pytest.raises(ConnectionException):
         proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
 
 
-def test_once_token_received_then_it_is_cached_for_subsequent_calls():
-    mocked_impl = generate_standard_mock()
-
-    proposal_system = YuosClient(SOME_URL, SOME_USER, SOME_PASSWORD, mocked_impl)
-
-    # Multiple calls
-    _ = proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
-    _ = proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
-    _ = proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
-
-    assert mocked_impl.get_token.call_count == 1
+# def test_once_token_received_then_it_is_cached_for_subsequent_calls():
+#     mocked_impl = generate_standard_mock()
+#
+#     proposal_system = YuosClient(SOME_URL, SOME_USER, SOME_PASSWORD, mocked_impl, SOME_TOKEN)
+#
+#     # Multiple calls
+#     _ = proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
+#     _ = proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
+#     _ = proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
+#
+#     assert mocked_impl.get_token.call_count == 1
 
 
 def test_querying_for_proposal_id_with_translates_instrument_name_into_correct_id():
     mocked_impl = generate_standard_mock()
 
-    proposal_system = YuosClient(SOME_URL, SOME_USER, SOME_PASSWORD, mocked_impl)
+    proposal_system = YuosClient(
+        SOME_URL, SOME_USER, SOME_PASSWORD, mocked_impl, SOME_TOKEN
+    )
     proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
 
     # This test is fragile because it relies on the order of the parameters
@@ -71,10 +77,7 @@ def test_instrument_list_is_called_if_empty_on_proposal_query():
     mocked_impl.get_instrument_data.return_value = VALID_INSTRUMENT_LIST
 
     proposal_system = YuosClient(
-        SOME_URL,
-        SOME_USER,
-        SOME_PASSWORD,
-        mocked_impl,
+        SOME_URL, SOME_USER, SOME_PASSWORD, mocked_impl, SOME_TOKEN
     )
 
     proposal_system.proposal_by_id("YMIR", VALID_PROPOSAL_ID)
