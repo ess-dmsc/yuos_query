d6406151d3038b45bb941b8c2fb58b80a23bb876
Matt Clarke <matt.clarke@esss.se>
2021-03-09 14:12:59 +0100

    Moved version

--- yuos_query/__init__.py
+++ yuos_query/__init__.py
@@ -2,5 +2,4 @@ from yuos_query.exceptions import BaseYuosException  # noqa
 from yuos_query.exceptions import ConnectionException  # noqa
 from yuos_query.exceptions import InvalidIdException  # noqa
 from yuos_query.proposal_system import YuosClient as YuosClient  # noqa
-
-__version__ = "0.1.8"
+from yuos_query.version import __version__  # noqa
