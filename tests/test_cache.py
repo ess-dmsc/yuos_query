ff8976bdda28ece2f414e6146ede8dfe55d474fd
Matt Clarke <matt.clarke@esss.se>
2021-05-10 11:21:44 +0200

    Add test for cache to check can extract proposals

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,5 +1,6 @@
 from unittest import mock
 
+from example_data import get_ymir_example_data
 from yuos_query.cache import Cache
 from yuos_query.proposal_system import _ProposalSystemWrapper
 
@@ -25,6 +26,8 @@ VALID_INSTRUMENT_LIST = [
     },
 ]
 
+YMIR_EXAMPLE_DATA = get_ymir_example_data()
+
 
 def test_refresh_cache_calls_proposal_system():
     impl = mock.create_autospec(_ProposalSystemWrapper)
@@ -35,3 +38,18 @@ def test_refresh_cache_calls_proposal_system():
 
     impl.get_instrument_data.assert_called_once()
     impl.get_proposals_including_samples_for_instrument.assert_called_once()
+
+
+def test_cache_gives_dictionary_of_proposals():
+    impl = mock.create_autospec(_ProposalSystemWrapper)
+    impl.get_instrument_data.return_value = VALID_INSTRUMENT_LIST
+    impl.get_proposals_including_samples_for_instrument.return_value = YMIR_EXAMPLE_DATA
+
+    cache = Cache(":: some_token ::", ":: some_url ::", "YMIR", implementation=impl)
+    cache.refresh()
+
+    assert len(cache.cached_proposals) == 17
+    assert (
+        cache.cached_proposals["471120"].title
+        == "The magnetic field dependence of the director state in the quantum spin hyperkagome compound Yb3Ga5O12"
+    )
