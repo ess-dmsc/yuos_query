36072eb127d2ae51c90eb5f1e9ce40a1b33c9758
Matt Clarke <matt.clarke@esss.se>
2021-05-10 11:34:11 +0200

    Missed file

---
+++ yuos_query/proposal_system.py
@@ -0,0 +1,96 @@
+from gql import Client, gql
+from gql.transport.requests import RequestsHTTPTransport
+
+
+class ProposalSystem:
+    """
+    Don't use this directly.
+    """
+
+    def execute_query(self, token, url, query_json):
+        """
+        Function for making a query on the proposal system.
+
+        :param token: the authorisation token
+        :param url: the proposal system URL
+        :param query_json: the query to make
+        :return: the JSON response
+        """
+        token = f"Bearer {token}"
+        transport = RequestsHTTPTransport(
+            url=url,
+            verify=True,
+            headers={"Authorization": token},
+        )
+        client = Client(transport=transport, fetch_schema_from_transport=True)
+        with client as session:
+            query = gql(query_json)
+            return session.execute(query)
+
+    def get_instrument_data(self, token, url):
+        json_data = self.execute_query(
+            token,
+            url,
+            """
+            {
+                instruments {
+                    instruments {
+                        id
+                        shortCode
+                        description
+                        name
+                    }
+                }
+            }
+            """,
+        )
+        return json_data["instruments"]["instruments"]
+
+    def get_proposals_including_samples_for_instrument(self, token, url, instrument_id):
+        json_data = self.execute_query(
+            token,
+            url,
+            """
+            {
+                proposals(filter: {instrumentId: $INST$}){
+                    totalCount
+                    proposals{
+                        shortCode
+                        title
+                        id
+                        users {
+                            firstname
+                            lastname
+                        }
+                        proposer {
+                            firstname
+                            lastname
+                        }
+                        samples {
+                            proposalId
+                            title
+                            id
+                            questionary {
+                                steps {
+                                    fields {
+                                        value
+                                        dependencies {
+                                            dependencyNaturalKey
+                                            questionId
+                                        }
+                                        question {
+                                            question
+                                            naturalKey
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            """.replace(
+                "$INST$", str(instrument_id)
+            ),
+        )
+        return json_data["proposals"]["proposals"]
