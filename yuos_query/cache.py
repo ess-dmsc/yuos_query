1b4fdd3d957e00de0643c2522bc1aefd90eb3e06
Ebad Kamil <ebad.kamil@ess.eu>
2021-05-18 14:44:13 +0200

    Add export method in cache

--- yuos_query/cache.py
+++ yuos_query/cache.py
@@ -5,3 +5,7 @@ class Cache:
 
     def update(self, proposals):
         self.proposals = proposals
+
+    def export_to_json(self, filename):
+        with open(filename, "w") as f:
+            f.write("LOL")
