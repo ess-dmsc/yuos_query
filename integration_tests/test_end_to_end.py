8745b5903ea7c9f2e5f78f6a50e4ad72f1359d28
Matt Clarke <matt.clarke@esss.se>
2021-05-20 15:28:38 +0200

    Fixed end to end test

--- integration_tests/test_end_to_end.py
+++ integration_tests/test_end_to_end.py
@@ -1,4 +1,5 @@
 import os
+from tempfile import TemporaryDirectory
 
 import pytest
 
@@ -17,33 +18,36 @@ SERVER_URL = "https://useroffice-test.esss.lu.se/graphql"
     SKIP_TEST, reason="no token supplied for testing against real system"
 )
 def test_get_proposals_and_sample_for_specific_id_on_ymir_instrument():
-    client = YuosClient(SERVER_URL, YUOS_TOKEN, "YMIR")
+    with TemporaryDirectory() as directory:
+        client = YuosClient(
+            SERVER_URL, YUOS_TOKEN, "YMIR", os.path.join(directory, "cache.json")
+        )
 
-    result = client.proposal_by_id("471120")
+        result = client.proposal_by_id("471120")
 
-    assert (
-        result.title
-        == "The magnetic field dependence of the director state in the quantum spin hyperkagome compound Yb3Ga5O12"
-    )
-    assert result.id == "471120"
-    assert result.users == [
-        ("jonathan ", "Taylor"),
-        ("Johan", "Andersson"),
-    ]
-    assert result.proposer == ("Fredrik", "Bolmsten")
-    assert len(result.samples) == 3
-    assert result.samples[0].name == ""
-    assert result.samples[0].formula == "Yb3Ga5O12"
-    assert result.samples[0].number == 1
-    assert result.samples[0].density == (0, "g/cm*3")
-    assert result.samples[0].mass_or_volume == (0, "")
-    assert result.samples[1].name == ""
-    assert result.samples[1].formula == "(EO)20-(PO)45-(EO)30, D2O, NaCl, SDS"
-    assert result.samples[1].number == 1
-    assert result.samples[1].density == (0, "g/cm*3")
-    assert result.samples[1].mass_or_volume == (0, "µg")
-    assert result.samples[2].name == ""
-    assert result.samples[2].formula == "PEO, D2O, NaCl, EtOH"
-    assert result.samples[2].number == 1
-    assert result.samples[2].density == (0, "g/cm*3")
-    assert result.samples[2].mass_or_volume == (0, "µg")
+        assert (
+            result.title
+            == "The magnetic field dependence of the director state in the quantum spin hyperkagome compound Yb3Ga5O12"
+        )
+        assert result.id == "471120"
+        assert result.users == [
+            ("jonathan ", "Taylor"),
+            ("Johan", "Andersson"),
+        ]
+        assert result.proposer == ("Fredrik", "Bolmsten")
+        assert len(result.samples) == 3
+        assert result.samples[0].name == ""
+        assert result.samples[0].formula == "Yb3Ga5O12"
+        assert result.samples[0].number == 1
+        assert result.samples[0].density == (0, "g/cm*3")
+        assert result.samples[0].mass_or_volume == (0, "")
+        assert result.samples[1].name == ""
+        assert result.samples[1].formula == "(EO)20-(PO)45-(EO)30, D2O, NaCl, SDS"
+        assert result.samples[1].number == 1
+        assert result.samples[1].density == (0, "g/cm*3")
+        assert result.samples[1].mass_or_volume == (0, "µg")
+        assert result.samples[2].name == ""
+        assert result.samples[2].formula == "PEO, D2O, NaCl, EtOH"
+        assert result.samples[2].number == 1
+        assert result.samples[2].density == (0, "g/cm*3")
+        assert result.samples[2].mass_or_volume == (0, "µg")
