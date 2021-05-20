435e061883e898780d7893f3678930eb35caece2
Ebad Kamil <ebad.kamil@ess.eu>
2021-05-20 11:21:26 +0200

    Add import from json in cache and tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -3,48 +3,20 @@ from tempfile import TemporaryDirectory
 
 from tests.test_yuos_client import VALID_PROPOSAL_DATA
 from yuos_query.cache import Cache
-from yuos_query.utils import (
-    deserialise_proposals_from_json,
-    serialise_proposals_to_json,
-)
 
 
 def test_exporting_data_to_json_from_cache():
     cache = Cache("YMIR")
+    cache.proposals = VALID_PROPOSAL_DATA
+
     filename = "test_filename.json"
 
     with TemporaryDirectory() as directory:
         filepath = os.path.join(directory, filename)
         cache.export_to_json(filepath)
         assert os.path.exists(filepath)
+        # Clear the cache
+        cache.proposals = {}
+        cache.import_from_json(filepath)
 
-
-def test_converting_proposals_to_json():
-    proposal_json = serialise_proposals_to_json(VALID_PROPOSAL_DATA)
-    proposals = deserialise_proposals_from_json(proposal_json)
-    assert proposals["471120"].id == VALID_PROPOSAL_DATA["471120"].id
-    assert proposals["471120"].title == VALID_PROPOSAL_DATA["471120"].title
-    assert proposals["471120"].proposer == VALID_PROPOSAL_DATA["471120"].proposer
-    assert proposals["471120"].users == VALID_PROPOSAL_DATA["471120"].users
-    assert proposals["471120"].db_id == VALID_PROPOSAL_DATA["471120"].db_id
-
-    assert (
-        proposals["471120"].samples[0].name
-        == VALID_PROPOSAL_DATA["471120"].samples[0].name
-    )
-    assert (
-        proposals["471120"].samples[0].formula
-        == VALID_PROPOSAL_DATA["471120"].samples[0].formula
-    )
-    assert (
-        proposals["471120"].samples[0].number
-        == VALID_PROPOSAL_DATA["471120"].samples[0].number
-    )
-    assert (
-        proposals["471120"].samples[0].mass_or_volume
-        == VALID_PROPOSAL_DATA["471120"].samples[0].mass_or_volume
-    )
-    assert (
-        proposals["471120"].samples[0].density
-        == VALID_PROPOSAL_DATA["471120"].samples[0].density
-    )
+        assert cache.proposals["471120"].id == VALID_PROPOSAL_DATA["471120"].id
--- yuos_query/cache.py
+++ yuos_query/cache.py
@@ -1,3 +1,9 @@
+from yuos_query.utils import (
+    deserialise_proposals_from_json,
+    serialise_proposals_to_json,
+)
+
+
 class Cache:
     def __init__(self, instrument):
         self.instrument = instrument
@@ -7,5 +13,9 @@ class Cache:
         self.proposals = proposals
 
     def export_to_json(self, filename):
-        with open(filename, "w") as f:
-            f.write("LOL")
+        with open(filename, "w") as file:
+            file.write(serialise_proposals_to_json(self.proposals))
+
+    def import_from_json(self, filename):
+        with open(filename, "r") as file:
+            self.proposals = deserialise_proposals_from_json(file.read())
