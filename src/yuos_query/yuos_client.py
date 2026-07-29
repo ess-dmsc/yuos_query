a3e0a9e793c64099957226292507ccaaca3c48c7
jekabskarklins <jekabs.karklins@ess.eu>
2026-07-29 13:01:16 +0200

    fix: replace logging with print statement for debugging purposes

--- src/yuos_query/yuos_client.py
+++ src/yuos_query/yuos_client.py
@@ -30,9 +30,7 @@ class YuosServer:
     def update_cache(self):
         try:
             proposals = self.system.get_proposals_for_instrument(self.instrument)
-            logging.info(
-                f"Retrieved {len(proposals)} proposals for instrument {self.instrument}"
-            )
+            print(f"Retrieved {len(proposals)} proposals for instrument {self.instrument}")
             self.cache.update(proposals)
             self.cache.export_to_file()
         except ServerException as error:
