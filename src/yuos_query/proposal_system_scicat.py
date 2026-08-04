36531142b756e409bbe98694df492b23466d807c
jekabskarklins <jekabs.karklins@ess.eu>
2026-08-04 11:00:07 +0200

    fix: format

--- src/yuos_query/proposal_system_scicat.py
+++ src/yuos_query/proposal_system_scicat.py
@@ -110,8 +110,12 @@ class ProposalRequester:
         for i in sorted(visitor_indices):
             first = str(self._meta_value(metadata, f"visitor_{i}_firstname")).strip()
             last = str(self._meta_value(metadata, f"visitor_{i}_lastname")).strip()
-            affiliation = str(self._meta_value(metadata, f"visitor_{i}_affiliation")).strip()
-            users.append(User(first, last, self._generate_fed_id(first, last), affiliation))
+            affiliation = str(
+                self._meta_value(metadata, f"visitor_{i}_affiliation")
+            ).strip()
+            users.append(
+                User(first, last, self._generate_fed_id(first, last), affiliation)
+            )
         return users
 
     def _extract_sample_id(self, sample: dict) -> str:
